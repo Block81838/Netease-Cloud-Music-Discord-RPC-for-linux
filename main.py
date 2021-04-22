57af8b2ab09dd68d840d8c7836ebef66887c2c37
Block81838 <haoxiangz201@gmail.com>
2021-04-22 04:57:09 -0700

    Fix bug
    * Fixed the program crashes when discord close but try to update the info

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import gi
+
 gi.require_version("Playerctl", "2.0")
 gi.require_version("GLib", "2.0")
 from gi.repository import Playerctl
@@ -6,20 +8,25 @@ from gi.repository import GLib
 
 from pypresence import Presence, InvalidID
 import time
+import os
 
 rpc = Presence("834396257312964608")
 manager = Playerctl.PlayerManager()
 
+
 def connect_to_rpc():
+	os.system(r"notify-send Discord\ RPC Connecting\ to\ Discord\ RPC")
 	while True:
 		try:
 			rpc.connect()
+			os.system(r"notify-send Discord\ RPC Connected")
 			break
 		except ConnectionRefusedError as e:
-			print("Failed to connect to discord rich presence! Try again in 10s")
+			os.system(r"notify-send Discord\ RPC Failed\ to\ connect\ to\ Discord\ RPC!\ Try\ again\ in\ 10s")
 			time.sleep(10)
 		except InvalidID as e:
-			print("Client ID is not valid, please check your client ID")
+			os.system(r"notify-send Discord\ RPC Client\ ID\ is not valid, Exiting")
+			exit(-1)
 
 
 def init_player(name):
@@ -43,6 +50,15 @@ def on_metadata(plr, metadata):
 	update_info(plr)
 
 
+def on_name_appear(plr, name):
+	if name.name == "netease-cloud-music":
+		init_player(name)
+
+
+def on_name_vanish(plr, name):
+	rpc.clear()
+
+
 def get_song_info(plr):
 	song_title = plr.get_title()
 	song_artist = plr.get_artist()
@@ -56,17 +72,26 @@ def update_info(plr):
 		status_mark = "playing"
 	elif status == 1 or status == 2:
 		status_mark = "pausing"
-	rpc.update(details=title, state=artist, large_image="neteaseicon", small_image=status_mark,\
-	           large_text="Netease Cloud Music", small_text=status_mark)
+	try:
+		rpc.update(details=title, state=artist, large_image="neteaseicon", small_image=status_mark, \
+	            large_text="Netease Cloud Music", small_text=status_mark)
+	except BrokenPipeError as e:
+		os.system("notify-send Discord\ RPC Lost\ connection\ to\ Discord,\ reconnecting")
+	except InvalidID as e:
+		os.system("notify-send Discord\ RPC Lost\ connection\ to\ Discord,\ reconnecting")
+		connect_to_rpc()
 
 
-connect_to_rpc()
+def main():
+	connect_to_rpc()
+	manager.connect("name-appeared", on_name_appear)
+	manager.connect("player-vanished", on_name_vanish)
 
-for player in manager.props.player_names:
-	init_player(player)
-
-GLib.MainLoop().run()
-
+	for player in manager.props.player_names:
+		if player.name == "netease-cloud-music":
+			init_player(player)
 
 
-
+if __name__ == "__main__":
+	main()
+	GLib.MainLoop().run()
